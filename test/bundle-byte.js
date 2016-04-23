ba19fcd2f38b3268644561d67675dc2ee7f58fdf
mkwtys <mkwtys@gmail.com>
2016-04-23 15:05:17 +0900

    feat: minify size

--- test/bundle-byte.js
+++ test/bundle-byte.js
@@ -3,8 +3,12 @@ const bundleBytes = require('../lib/bundle-bytes');
 
 describe('bundle-bytes', function() {
   it('noop - browser-pack code only', function() {
-    return bundleBytes('./example/noop').then(function(bytes) {
-      assert.equal(bytes, 566);
+    return bundleBytes('./example/noop').then(function(results) {
+      assert.deepEqual(results, {
+        modules: ['./example/noop'],
+        bundle: 566,
+        minify: 557
+      });
     });
   });
 });
